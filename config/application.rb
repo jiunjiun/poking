bb5ae18b937a219d88cf93f5998a202fad2a5239
jiunjiun <quietmes@gmail.com>
2018-07-09 14:31:18 +0800

    Refactor - Refine application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,20 @@ module Poking
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = 'zh-TW'
+    config.time_zone = 'Taipei'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
