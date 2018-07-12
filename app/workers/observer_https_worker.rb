a9e6a58ca3e2e23b513201b581c56c4a394d672e
jiunjiun <quietmes@gmail.com>
2018-07-13 04:10:29 +0800

    Refactor - Refine https worker

--- app/workers/observer_https_worker.rb
+++ app/workers/observer_https_worker.rb
@@ -3,39 +3,58 @@ class ObserverHttpsWorker
   include Sidekiq::Status::Worker
 
   sidekiq_options retry: 0
-  sidekiq_options queue: 'haha'
+  # sidekiq_options queue: 'haha'
 
   def perform observer_id, opts={}
     observer = Observer.find_by_id observer_id
 
     if observer.present?
-      event_type    = ObserverEvent::Type::DOWN
+      event_type    = nil
       response      = nil
       response_time = nil
       response_code = nil
+      time          = nil
+      reason        = nil
 
       begin
         time = Benchmark.measure do
           response = RestClient::Request.execute(method: observer.http_method, url: observer.url, timeout: 120)
         end
-        Rails.logger.debug { " -- time: #{time}" }
-        Rails.logger.debug { " -- time: #{time.real}" }
-
-        event_type = ObserverEvent::Type::UP
+        event_type    = ObserverEvent::Type::UP
         response_time = time.real
         response_code = response.code
+        reason = "OK (#{response_code})"
+
+      rescue RestClient::ExceptionWithResponse => e
+        event_type = ObserverEvent::Type::DOWN
+        response_code = e.response.try(:code)
+        reason = e
+
+      rescue RestClient::RequestFailed => e
+        event_type = ObserverEvent::Type::DOWN
+        response_code = e.response.try(:code)
+        reason = e
+
       rescue Exception => e
-        Rails.logger.debug { "[ObserverHttpsWorker]: #{e}" }
+        Rails.logger.debug { "[ObserverHttpsWorker][e]: #{e}" }
+
+        event_type = ObserverEvent::Type::DOWN
+        response_code = e.try(:response).try(:code)
+        reason = e
       end
 
-
-      observer.events.create({
-        event_type: event_type,
-        response_time: response_time,
-        response_code: response_code,
-      })
-
       if observer.status == Observer::Status::STARTED
+        observer.events.create({
+          event_type: event_type,
+          reason: reason
+        })
+
+        observer.records.create({
+          event_type: event_type,
+          response_time: response_time,
+          response_code: response_code,
+        })
+
         ObserverHttpsWorker.perform_in(observer.interval.to_i.minutes, observer.id)
       end
     end
