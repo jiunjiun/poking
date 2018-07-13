7e2541a08fa58f703322accdb34bb6bd9b25bf64
jiunjiun <quietmes@gmail.com>
2018-07-14 02:00:59 +0800

    Refactor - Add timezone settings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,9 +4,16 @@ class AdminController < ApplicationController
   layout 'admin'
 
   before_action :authenticate_user!
+  before_action :set_timezone
   before_action :update_meta
 
   private
+  def set_timezone
+    if current_user && current_user.time_zone
+      Time.zone = current_user.time_zone
+    end
+  end
+
   def update_meta
     meta_tags_option = {
       site: I18n.t('backend'),
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Poking
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = 'zh-TW'
-    config.time_zone = 'Taipei'
+    # config.time_zone = 'Taipei'
 
     config.generators do |g|
       g.test_framework :rspec,
